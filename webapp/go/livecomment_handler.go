aa2ea8b98eeeba64c068217455ba4bf85e0d9c40
ryo0210 <yone0217@icloud.com>
2024-12-15 20:53:50 +0900

    スコア: 4433 → 4385　/moderateのNGワードのコメント削除

--- webapp/go/livecomment_handler.go
+++ webapp/go/livecomment_handler.go
@@ -386,32 +386,37 @@ func moderateHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get NG words: "+err.Error())
 	}
 
-	// NGワードにヒットする過去の投稿も全削除する
-	for _, ngword := range ngwords {
-		// ライブコメント一覧取得
-		var livecomments []*LivecommentModel
-		if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments"); err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livecomments: "+err.Error())
-		}
-
-		for _, livecomment := range livecomments {
-			query := `
-			DELETE FROM livecomments
-			WHERE
-			id = ? AND
-			livestream_id = ? AND
-			(SELECT COUNT(*)
-			FROM
-			(SELECT ? AS text) AS texts
-			INNER JOIN
-			(SELECT CONCAT('%', ?, '%')	AS pattern) AS patterns
-			ON texts.text LIKE patterns.pattern) >= 1;
-			`
-			if _, err := tx.ExecContext(ctx, query, livecomment.ID, livestreamID, livecomment.Comment, ngword.Word); err != nil {
-				return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
-			}
-		}
+	// あまり、スコア上がってない。なんなら下がってる。時間でいうと中央値5sくらいのが1sかからないくらいくらいになっているので、残しておく
+	if _, err := tx.ExecContext(ctx, `DELETE FROM livecomments WHERE livestream_id = ? AND comment LIKE CONCAT('%', ?, '%')`, livestreamID, req.NGWord); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
 	}
+
+	// NGワードにヒットする過去の投稿も全削除する
+	// for _, ngword := range ngwords {
+	// 	// ライブコメント一覧取得
+	// 	var livecomments []*LivecommentModel
+	// 	if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments"); err != nil {
+	// 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livecomments: "+err.Error())
+	// 	}
+
+	// 	for _, livecomment := range livecomments {
+	// 		query := `
+	// 		DELETE FROM livecomments
+	// 		WHERE
+	// 		id = ? AND
+	// 		livestream_id = ? AND
+	// 		(SELECT COUNT(*)
+	// 		FROM
+	// 		(SELECT ? AS text) AS texts
+	// 		INNER JOIN
+	// 		(SELECT CONCAT('%', ?, '%')	AS pattern) AS patterns
+	// 		ON texts.text LIKE patterns.pattern) >= 1;
+	// 		`
+	// 		if _, err := tx.ExecContext(ctx, query, livecomment.ID, livestreamID, livecomment.Comment, ngword.Word); err != nil {
+	// 			return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
+	// 		}
+	// 	}
+	// }
 
 	if err := tx.Commit(); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to commit: "+err.Error())
